9e0cb5d3494d9a861011d3399807dd7e67b9481f
Alfredo Lopez <soysidarg@gmail.com>
2026-07-22 01:20:21 -0300

    Escalar extraccion a multiples productos con for; resolver ambiguedad de selector en productos con descuento

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from playwright.sync_api import sync_playwright
 
+lista_precios=[]
+
+productos = [
+    {"nombre": "Yerba", "url": "https://www.carrefour.com.ar/yerba-mate-sabor-hierbas-buenas-y-santas-500-grs-718429/p"},
+    {"nombre": "Aceite", "url": "https://www.carrefour.com.ar/aceite-de-girasol-natura-15-l/p"},
+    {"nombre": "Leche", "url": "https://www.carrefour.com.ar/leche-la-serenisima-clasica-3-1l-720719/p"}
+
+]
+
 def limpiar_funcion(dato):
     dato=dato.replace("$","")
     dato=dato.replace(".","")
@@ -14,8 +23,14 @@ with sync_playwright() as p:
         user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
     )
     page = context.new_page()
-    page.goto("https://www.carrefour.com.ar/yerba-mate-sabor-hierbas-buenas-y-santas-500-grs-718429/p")
-    dato = page.locator("span.valtech-carrefourar-product-price-0-x-sellingPrice").inner_text()
-        
-    precio_actual=limpiar_funcion(dato)
-    print(precio_actual)
+
+    for producto in productos:
+        page.goto(producto["url"])
+        contenedor = page.locator("span.valtech-carrefourar-product-price-0-x-sellingPrice")
+        dato = contenedor.locator("span.valtech-carrefourar-product-price-0-x-currencyContainer").inner_text()
+        precio_actual=limpiar_funcion(dato)
+        lista_precios.append({"nombre":producto["nombre"],"precio":precio_actual})
+
+    print(lista_precios)
+
+    browser.close()
