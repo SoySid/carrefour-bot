36296f8558525660d6770c0a6395e309cb2c909a
Alfredo Lopez <soysidarg@gmail.com>
2026-07-22 15:58:52 -0300

    Fase 5: calcular inflacion acumulada por producto y variacion total de la canasta

--- main.py
+++ main.py
@@ -20,6 +20,28 @@ def limpiar_funcion(dato):
     precio=float(dato)
     return precio
 
+def calcular_variacion(dato1,dato2):
+    variacion=((dato1-dato2)/dato2)*100
+    variacion=round(variacion,2)
+    return variacion
+
+def calcular_estado(variacion):
+    if variacion>0:
+        estado="subio"
+    elif variacion<0:
+        estado="bajo"
+    else:
+        estado="igual"
+    return estado
+
+def calcular_variacion_canasta(lista1,lista2):
+    precios_hoy=sum(lista1)
+    precios_anterior=sum(lista2)
+
+    variacion= calcular_variacion(precios_hoy,precios_anterior)
+    return variacion
+
+
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False, slow_mo=1000)
     context = browser.new_context(
@@ -50,22 +72,32 @@ with sync_playwright() as p:
         with open("historial_precios.json", "r") as archivo:
             datos = json.load(archivo)
             registro_ayer=datos[-1]
+            registro_dia1=datos[0]["registros"]
+            lista_precios_hoy = [item["precio"] for item in lista_precios]
+            lista_precios_dia1 = [item["precio"] for item in datos[0]["registros"]]
+            lista_precios_ayer = [item["precio"] for item in datos[-1]["registros"]]
+
         for lista in lista_precios:
             nombre_buscado=lista["nombre"]
             precio_hoy=lista["precio"]
-            
+
+            for registro in registro_dia1:
+                precio_dia1=registro["precio"]
+                
+                if nombre_buscado==registro["nombre"]:
+                   precio_dia1=registro["precio"]
+                   variacion_acumulada=calcular_variacion(precio_hoy,precio_dia1)
+                   estado_acumulado=calcular_estado(variacion_acumulada)
+
             for registro in registro_ayer["registros"]:
                 if nombre_buscado==registro["nombre"]:
                     precio_ayer=registro["precio"]
-                    variacion = ((precio_hoy - precio_ayer) / precio_ayer) * 100
-                    variacion=round(variacion,2)
-                    if variacion>0:
-                        estado="subio"
-                    elif variacion<0:
-                        estado="bajo"
-                    else:
-                        estado="igual"
-            print(f"Producto: {nombre_buscado}, precio ayer: {precio_ayer}, precio hoy: {precio_hoy}, variacion: {variacion}, estado: {estado}")
+                    variacion_dia=calcular_variacion(precio_hoy,precio_ayer)
+                    estado_dia=calcular_estado(variacion_dia)
+            
+            print(f"Producto: {nombre_buscado}, precio ayer: {precio_ayer}, precio hoy: {precio_hoy}, variacion del dia: {variacion_dia}, estado: {estado_dia}, variacion acumulada:{variacion_acumulada}, estado:{estado_acumulado}")
+        print(f"Variacion de la canasta en el dia:{calcular_variacion_canasta(lista_precios_hoy,lista_precios_ayer)}")
+        print(f"Variacion de la canasta acumulada:{calcular_variacion_canasta(lista_precios_hoy,lista_precios_dia1)}")
         datos.append(registro_hoy)
         with open(nombre_archivo, "w") as archivo:
             json.dump(datos, archivo)
